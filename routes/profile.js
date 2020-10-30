9a3115222b19d54a27e96c331d839dee9ee3e7e9
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-10-30 09:58:29 -0400

    fixed query

--- routes/profile.js
+++ routes/profile.js
@@ -4,45 +4,51 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 // view profile page
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   res.render("profile/profile");
 })
 
 // register
-router.get('/register', (req,res) => {
+router.get('/register', (req, res) => {
   res.render("profile/register");
 })
-router.post('/register', async (req,res) => {
+router.post('/register', async (req, res) => {
   name = req.body.name;
   email = req.body.email;
   pass = req.body.password;
-  if(!name || !email || !pass){
+  if (!name || !email || !pass) {
     console.log("missing info");
-    // invalid msg (missing info)!!!!!
+    // invalid msg flash (missing info)!!!!!
     res.redirect('/profile/login')
     return;
   }
-    user = User.findOne({email: email})
-    if(user){
+  User.findOne({email: email}, async (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (user) {
         console.log("user exists")
         res.redirect('/profile/login')
-    }else{
-    password = await bcrypt.hash(pass, 10)
-    user = new User({
-      name: req.body.name,
-      email:req.body.email,
-      password: pass
-    })
-    await user.save();
-    res.redirect('/profile')
-  }
+        return;
+      } else {
+        password = await bcrypt.hash(pass, 10);
+        user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: pass
+        })
+        await user.save();
+        res.redirect('/profile');
+      }
+    }
+  })
 })
 
 // login
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
   res.render("profile/login");
 })
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
   res.render("profile/login");
 })
 
